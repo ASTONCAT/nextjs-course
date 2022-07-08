6c1eef1eb3894d294c62f8f6ce1f330b68daef29
Koder Astoncat <koder@astoncat.com>
2022-07-08 09:30:28 +0200

    Extracting Dynamic Route Data

--- pages/news/[newsid].js
+++ pages/news/[newsid].js
@@ -1,6 +1,15 @@
+import { useRouter } from 'next/router'
+
 // our-domain.com/news/nejaka-zprava
 
 function NejakaZprava() {
+	const router = useRouter()
+
+	const newsId = router.query.newsid
+
+	// send a request to the backend API
+	// to fetch the news item with newsId
+
 	return <h1>Nějaká Zpráva</h1>
 }
 
